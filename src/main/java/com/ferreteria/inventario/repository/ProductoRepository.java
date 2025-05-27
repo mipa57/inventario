4a698ff6f3a941421a4d5e148ad07e2490d4b7bf
Miguel Bejarano <mipa57125@gmail.com>
2025-05-27 10:39:01 -0500

    Se cambio a finBYNombre pues daba error

--- src/main/java/com/ferreteria/inventario/repository/ProductoRepository.java
+++ src/main/java/com/ferreteria/inventario/repository/ProductoRepository.java
@@ -1,9 +1,11 @@
 package com.ferreteria.inventario.repository;
 
-import com.ferreteria.inventario.model.Producto;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ferreteria.inventario.model.Producto;
+
 public interface ProductoRepository extends MongoRepository<Producto, String> {
-    Optional<Producto> findByCodigo(String codigo);
+    Optional<Producto> findByNombre(String nombre);
 }
