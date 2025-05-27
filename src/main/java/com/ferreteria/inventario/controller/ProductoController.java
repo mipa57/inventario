66ae75b96bd7ea803e6463ebe6b4e156df00bb44
Miguel Bejarano <mipa57125@gmail.com>
2025-05-27 12:23:33 -0500

    Se hizo cambios a productocontroller

--- src/main/java/com/ferreteria/inventario/controller/ProductoController.java
+++ src/main/java/com/ferreteria/inventario/controller/ProductoController.java
@@ -1,15 +1,24 @@
-﻿package com.ferreteria.inventario.controller;
+package com.ferreteria.inventario.controller;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ferreteria.inventario.model.Producto;
 import com.ferreteria.inventario.repository.ProductoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import jakarta.validation.Valid;
-import org.springframework.validation.BindingResult;
 
-import java.util.List;
-import java.util.Optional;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/productos")
