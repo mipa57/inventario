c2a39eaa73abf3ce63fe1307f9896cb48edd66b2
Miguel Bejarano <mipa57125@gmail.com>
2025-05-30 11:29:27 -0500

    Se actualizo inventaioaplication

--- src/main/java/com/ferreteria/inventario/InventarioApplication.java
+++ src/main/java/com/ferreteria/inventario/InventarioApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class
 InventarioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InventarioApplication.class, args);
 	}
-
 }
